6741ce3cf3e41595770281f45fe5c09aa6911ba6
Josué Camelo <cloudone@josuecamelo.com>
2022-09-20 16:45:53 -0300

    Herança de Template com block

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from recipes.views import home
+#from recipes.views import home  # ao inves de importar vários posso importar todas as views de recipes
+from . import views
 
 urlpatterns = [
-    path('', home), #Home
+    path('', views.home), #Home
+    path('recipes/<int:id>/', views.recipe), #detalhes
 ]
