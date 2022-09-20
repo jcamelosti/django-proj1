f52e086e2ba48c10a0ae143ec34e99887af3c1d2
Josué Camelo <cloudone@josuecamelo.com>
2022-09-20 14:46:58 -0300

    Estrutura Básica do Layout

--- recipes/views.py
+++ recipes/views.py
@@ -4,12 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Josué Camelo'
     })
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
