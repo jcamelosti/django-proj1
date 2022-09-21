b8b4622c54ae3df6fe55fdef62c6d765feca459d
Josué Camelo <cloudone@josuecamelo.com>
2022-09-21 11:12:51 -0300

    Correções

--- recipes/views.py
+++ recipes/views.py
@@ -1,33 +1,30 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-from recipes.models import Recipe
+from .models import Recipe
 
-# Create your views here.
+# from utils.recipes.factory import make_recipe
+
+
 
 def home(request):
-    # recipes = Recipe.objects.all().order_by('-id')
-    recipes = Recipe.objects.filter(
-        is_published=True
-    ).order_by('-id')
-
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes
+        'recipes': recipes,
     })
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-
-    return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes
+    recipes = Recipe.objects.filter(
+        category__id=category_id
+    ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
     })
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(pk=id)
-    
+    recipe = Recipe.objects.get(pk=id)
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
-        'is_detail_page': True
+        'is_detail_page': True,
     })
