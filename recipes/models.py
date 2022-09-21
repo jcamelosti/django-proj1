d6631f11bfbb99c453fa57f300183ac193168c18
Josué Camelo <cloudone@josuecamelo.com>
2022-09-20 23:53:34 -0300

    Path de Imagens

--- recipes/models.py
+++ recipes/models.py
@@ -7,6 +7,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=255)
@@ -28,6 +31,9 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.title
 
 # EDITED
 # title description slug
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 # from recipes.views import home  # ao inves de importar vários posso importar todas as views de recipes
 from . import views
@@ -10,3 +12,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
