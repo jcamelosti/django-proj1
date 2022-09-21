d04ad4e094d624f5d9e90b5dce8709ca7ff4ec11
Josué Camelo <cloudone@josuecamelo.com>
2022-09-20 23:05:05 -0300

    Url únicas e app_name

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-#from recipes.views import home  # ao inves de importar vários posso importar todas as views de recipes
+# from recipes.views import home  # ao inves de importar vários posso importar todas as views de recipes
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home), #Home
-    path('recipes/<int:id>/', views.recipe), #detalhes
+    path('', views.home, name="recipes"),  # Home
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
