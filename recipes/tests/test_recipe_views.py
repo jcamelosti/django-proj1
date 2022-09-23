4084b90ee1ca7b21bdcdb28a7084fb9ea8194e53
Josué Camelo <cloudone@josuecamelo.com>
2022-09-23 11:25:05 -0300

    Correção Teste

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -40,9 +40,9 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_category_template_dont_load_recipes_not_published(self):
         """Test recipe is_published False dont show"""
         # Need a recipe for this test
-        self.make_recipe(is_published=False)
+        recipe = self.make_recipe(is_published=False)
 
-        response = self.client.get(reverse('recipes:recipe', kwargs={'id' : 1}))
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id' : recipe.category.id}))
 
         self.assertEqual(response.status_code, 404)
 
