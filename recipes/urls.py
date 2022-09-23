34564f451b5fe2e46b4bc4669649e36eea549d43
Josué Camelo <cloudone@josuecamelo.com>
2022-09-23 13:02:01 -0300

    Testando Recursos

--- recipes/urls.py
+++ recipes/urls.py
@@ -9,10 +9,10 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('recipes/search/', lambda request: ..., name="search"),
     path('recipes/category/<int:category_id>/',
          views.category, name="category"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
-    path('recipes/search/', lambda request: ..., name="search"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
