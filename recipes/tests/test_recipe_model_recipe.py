f7f1dd14517a3375e25de659d91910c8b6f0dc50
Josué Camelo <cloudone@josuecamelo.com>
2022-09-23 13:48:56 -0300

    Indicando View para retornar template correto

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -26,30 +26,30 @@ class RecipeModelTest(RecipeTestBase):
         recipe.save()
         return recipe
 
-    #@parameterized.expand([
-    #    ('title', 65),
-    #    ('description', 165),
-    #    ('preparation_time_unit', 65),
-    #    ('servings_unit', 65),
-    #])
-    #def test_recipe_fields_max_length(self, field, max_length):
-    #    setattr(self.recipe, field, 'A' * (max_length + 1))
-    #    with self.assertRaises(ValidationError):
-    #        self.recipe.full_clean()
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
 
-    #def test_recipe_preparation_steps_is_html_is_false_by_default(self):
-    #    recipe = self.make_recipe_no_defaults()
-    #    self.assertFalse(
-    #        recipe.preparation_steps_is_html,
-    #        msg='Recipe preparation_steps_is_html is not False',
-    #    )
+    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(
+            recipe.preparation_steps_is_html,
+            msg='Recipe preparation_steps_is_html is not False',
+        )
 
-    #def test_recipe_is_published_is_false_by_default(self):
-    #    recipe = self.make_recipe_no_defaults()
-    #    self.assertFalse(
-    #        recipe.is_published,
-    #        msg='Recipe is_published is not False',
-    #    )
+    def test_recipe_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(
+            recipe.is_published,
+            msg='Recipe is_published is not False',
+        )
 
     def test_recipe_string_representation(self):
         needed = 'Testing Representation'
