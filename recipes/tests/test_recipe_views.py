4efe116205d3c67bb38bb7b0a700619248254d94
Josué Camelo <cloudone@josuecamelo.com>
2022-09-28 22:17:20 -0300

    Exibindo 404 Quando Termo não Existir

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -139,5 +139,6 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
 
     def test_recipe_search_raises_404_if_no_search_term(self):
-        response = self.client.get(reverse('recipes:search'))
+        url = reverse('recipes:search')  # + '?q=teste'
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
