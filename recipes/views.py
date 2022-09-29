6007a6019a567dd6917326ff04e13cb7b2a7d0bb
Josué Camelo <cloudone@josuecamelo.com>
2022-09-28 22:55:18 -0300

    Filtro Complexo com Q icontains

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,8 @@
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from .models import Recipe
-
-# from utils.recipes.factory import make_recipe
 
 
 def home(request):
@@ -64,7 +63,13 @@ def search(request):
     if not search_term:
         raise Http404()
 
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
+        'recipes': recipes,
     })
