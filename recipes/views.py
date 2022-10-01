cc445dfb1bd460588d86a3e977df2217a7a8be6e
Josué Camelo <cloudone@josuecamelo.com>
2022-10-01 15:17:32 -0300

    Formantando Flash Messages

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,5 @@
 import os
 
-from django.contrib import messages
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
@@ -17,11 +16,6 @@ def home(request):
     ).order_by('-id')
 
     page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
-
-    messages.error(request, 'teste')
-    messages.info(request, 'teste')
-    messages.warning(request, 'teste')
-    messages.success(request, 'teste')
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
