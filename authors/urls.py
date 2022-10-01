d7ad227949acd2c7c4f25c2570ea17dc58960709
Josué Camelo <cloudone@josuecamelo.com>
2022-10-01 15:34:10 -0300

    Ajustando URLS

--- authors/urls.py
+++ authors/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 
-...
+from . import views
+
+app_name = 'authores'
+
+urlpatterns = [
+    path('register/', views.register_view, name="register_view"),
+]
--- authors/views.py
+++ authors/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def register_view(request):
-    render(request, 'authors/pages/register_view.html')
+    return render(request, 'authors/pages/register_view.html')
