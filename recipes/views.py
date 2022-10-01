a0d9008ca80f36a8267ae8406044774e9ab1bb1a
Josué Camelo <cloudone@josuecamelo.com>
2022-10-01 14:30:59 -0300

    Variaveis de Ambiente

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,5 @@
 import os
 
-from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
