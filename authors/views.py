6678994bbe693dab59c0fbef4eb711ec7aa9455b
Josué Camelo <cloudone@josuecamelo.com>
2022-10-04 13:40:29 -0300

    Corrigindo a criptografia de senha do usuário registrado pela nossa aplicação

--- authors/views.py
+++ authors/views.py
@@ -2,14 +2,12 @@ from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
 
-from authors.forms import RegisterForm
+from .forms import RegisterForm
 
 
-# Create your views here.
 def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
     form = RegisterForm(register_form_data)
-
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
@@ -24,8 +22,11 @@ def register_create(request):
     form = RegisterForm(POST)
 
     if form.is_valid():
-        form.save()
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
         messages.success(request, 'Your user is created, please log in.')
+
         del (request.session['register_form_data'])
 
     return redirect('authors:register')
