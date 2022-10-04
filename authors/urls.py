70bbe3f228534707e81746ffc7e94c0a8025e4c6
Josué Camelo <cloudone@josuecamelo.com>
2022-10-04 11:11:08 -0300

    Enviando Post para OUtra URL usando session

--- authors/urls.py
+++ authors/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name = 'authores'
+app_name = 'authors'
 
 urlpatterns = [
-    path('register/', views.register_view, name="register_view"),
+    path('register/', views.register_view, name="register"),
+    path('register/create/', views.register_create, name="create"),
 ]
