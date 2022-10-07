c14418a919d88b72a7fa2d73b6e8a96fd31f58a2
Josué Camelo <cloudone@josuecamelo.com>
2022-10-07 13:45:20 -0300

    Usando mixins para compartilhar métodos entre classes

--- tests/function_tests/recipes/test_recipe_home_page.py
+++ tests/function_tests/recipes/test_recipe_home_page.py
@@ -7,6 +7,7 @@ from .base import RecipeBaseFunctionalTest
 @pytest.mark.function_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipes_not_found_message(self):
+        # self.make_recipe()
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('No recipes found here 🥲', body.text)
