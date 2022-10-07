c6ba6aaac475b1f5bf67c69e06dbce115b106c85
Josué Camelo <cloudone@josuecamelo.com>
2022-10-07 13:14:46 -0300

    Criando Classe Base de Teste

--- utils/browser.py
+++ utils/browser.py
@@ -1,4 +1,5 @@
 # from pathlib import Path
+import os
 from time import sleep
 
 from selenium import webdriver
@@ -16,6 +17,9 @@ def make_chrome_browser(*options):
     if options is not None:
         for option in options:
             chrome_options.add_argument(option)
+
+    if os.environ.get('SELENIUM_HEADLESS') == '1':
+        chrome_options.add_argument('--headless')
 
     chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
     browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
