ba21a6192e7c21e2b6b86eefab7e5d87abe559d0
Josué Camelo <cloudone@josuecamelo.com>
2022-10-15 23:02:08 -0300

    ListView: criando uma base para Home, Category e Search de recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,41 @@
 import os
+from typing import List
 
 from django.db.models import Q
-from django.http import Http404
+from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.views.generic import ListView
 from utils.pagination import make_pagination
 
-from .models import Recipe
+from recipes.models import Recipe
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
+
+
+class RecipeListViewBase(ListView):
+    model = Recipe
+    context_object_name = 'recipes'
+    ordering = ['-id']
+    template_name = 'recipes/pages/home.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            is_published=True,
+        )
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_obj, pagination_range = make_pagination(
+            self.request,
+            ctx.get('recipes'),
+            PER_PAGE
+        )
+        ctx.update(
+            {'recipes': page_obj, 'pagination_range': pagination_range}
+        )
+        return ctx
 
 
 def home(request):
@@ -24,17 +52,6 @@ def home(request):
 
 
 def category(request, category_id):
-    # recipes = Recipe.objects.filter(
-    #    category__id=category_id
-    # ).order_by('-id')
-
-    # if not recipes:
-    #    raise Http404('Página não encontrada.')
-
-    # return render(request, 'recipes/pages/category.html', context={
-    #    'recipes': recipes,
-    #    'title': f'{recipes.first().category.name} - Category | '
-    # })
     recipes = get_list_or_404(
         Recipe.objects.filter(
             category__id=category_id,
@@ -52,16 +69,7 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    # recipe = Recipe.objects.get(pk=id)
-    # recipe = Recipe.objects.filter(
-    #    pk=id,
-    #    is_published=True,
-    # ).order_by('-id').first()
-
-    recipe = get_object_or_404(Recipe.objects.filter(
-        pk=id,
-        is_published=True,
-    ))
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
@@ -76,8 +84,10 @@ def search(request):
         raise Http404()
 
     recipes = Recipe.objects.filter(
-        Q(title__icontains=search_term) |
-        Q(description__icontains=search_term),
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
         is_published=True
     ).order_by('-id')
 
@@ -88,5 +98,5 @@ def search(request):
         'search_term': search_term,
         'recipes': page_obj,
         'pagination_range': pagination_range,
-        'additional_url_query': f'&q={search_term}'
+        'additional_url_query': f'&q={search_term}',
     })
