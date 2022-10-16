fe1b69b07efbfe50374f7f19f6bf624fc25357e4
Josué Camelo <cloudone@josuecamelo.com>
2022-10-15 23:06:40 -0300

    Category ListView

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
 import os
-from typing import List
 
 from django.db.models import Q
 from django.http.response import Http404
@@ -35,6 +34,48 @@ class RecipeListViewBase(ListView):
         ctx.update(
             {'recipes': page_obj, 'pagination_range': pagination_range}
         )
+        return ctx
+
+
+class RecipeListViewHome(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+
+class RecipeListViewCategory(RecipeListViewBase):
+    template_name = 'recipes/pages/category.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            category__id=self.kwargs.get('category_id')
+        )
+        return qs
+
+
+class RecipeListViewSearch(RecipeListViewBase):
+    template_name = 'recipes/pages/search.html'
+
+    def get_queryset(self, *args, **kwargs):
+        search_term = self.request.GET.get('q', '')
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            Q(
+                Q(title__icontains=search_term) |
+                Q(description__icontains=search_term),
+            )
+        )
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        search_term = self.request.GET.get('q', '')
+
+        ctx.update({
+            'page_title': f'Search for "{search_term}" |',
+            'search_term': search_term,
+            'additional_url_query': f'&q={search_term}',
+        })
+
         return ctx
 
 
