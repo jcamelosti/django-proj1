d32e0d1c3c3d06814e484357904769ab48e17aad
Josué Camelo <cloudone@josuecamelo.com>
2022-10-22 14:32:01 -0300

    Atualizando Projeto

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,24 +1,13 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
-# from recipes.views import home  # ao inves de importar vários posso importar todas as views de recipes
 from . import views
 
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.RecipeListViewHome.as_view(), name="home"),
-    path(
-        'recipes/search/',
-        views.RecipeListViewSearch.as_view(), name="search"
-    ),
-    path(
-        'recipes/category/<int:category_id>/',
-        views.RecipeListViewCategory.as_view(), name="category"
-    ),
-    path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name="recipe"),
+    path('', views.home, name="home"),
+    path('recipes/search/', views.search, name="search"),
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
